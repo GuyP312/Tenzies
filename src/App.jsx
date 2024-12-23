e91948e2021399d793b8e66cdf0468cc401892ed
GuyP <guyparnchinda@gmail.com>
2024-12-23 23:25:18 +0700

    assign the dicevalues array to a react state. Then map over the state to create the Die component that sent in the random value as props. THerefore, the die now will display the random value

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,24 @@
 import React from 'react';
 import Die from './Components/Die';
-
-
-function generateAllValueDice(){ // return an array of random numbers between 1-6 of length 10
-  const dicevalue = [];
-  for(let i=0; i<10;i++){
-    const randomnum = Math.floor(Math.random() * 6) + 1; // creating random between 1-6 (+1 because without +1, it will randomize 0-5)
-    dicevalue.push(randomnum);
-  }
-  return dicevalue;
-}
-
 export default function App(){
+  function generateAllValueDice(){ // return an array of random numbers between 1-6 of length 10
+    const dicevalue = [];
+    for(let i=0; i<10;i++){
+      const randomnum = Math.floor(Math.random() * 6) + 1; // creating random number between 1-6 (+1 because without +1, it will randomize 0-5)
+      dicevalue.push(randomnum);
+    }
+    return dicevalue;
+  }
+  const [dicevalues, setdicevalues] = React.useState(generateAllValueDice);
+  const displaydice = dicevalues.map((die)=>{
+    return <Die number={die}/>
+  })
+
   return (
     <>
       <main>
         <div className = "dice-container">
-        <Die number={1} />
-        <Die number={2} />
-        <Die number={3} />
-        <Die number={4} />
-        <Die number={5} />
-        <Die number={6} />
-        <Die number={7} />
-        <Die number={8} />
-        <Die number={9} />
-        <Die number={10} />
+          {displaydice}
         </div>
       </main>
     </>
