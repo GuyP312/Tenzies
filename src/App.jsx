e0dd7338ddf45e93f7d66f635379974c7e0ef0ec
GuyP <guyparnchinda@gmail.com>
2024-12-24 02:38:42 +0700

    update the value inside array to be an object instead

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,16 @@ export default function App(){
     const dicevalue = [];
     for(let i=0; i<10;i++){
       const randomnum = Math.floor(Math.random() * 6) + 1; // creating random number between 1-6 (+1 because without +1, it will randomize 0-5)
-      dicevalue.push(randomnum);
+      dicevalue.push({value:randomnum,isHeld: false});
     }
     return dicevalue;
   }
   const [dicevalues, setdicevalues] = React.useState(generateAllValueDice); // set the array value to equal a state and then map it and send in random value as prop
   const displaydice = dicevalues.map((die)=>{
-    return <Die number={die}/>
+    return <Die number={die.value}/>
   })
 
-  function newdice(){
+  function newdice(){ // set the array to have new value and then update state
     setdicevalues(generateAllValueDice)
   }
 
