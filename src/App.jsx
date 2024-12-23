2394cc3808152593c4bcfd1d56ee2e003a4fcf9d
GuyP <guyparnchinda@gmail.com>
2024-12-23 23:14:32 +0700

    add a function that create an array of random numbers between 1-6 of length 10

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import Die from './Components/Die';
 
+
+function generateAllValueDice(){ // return an array of random numbers between 1-6 of length 10
+  const dicevalue = [];
+  for(let i=0; i<10;i++){
+    const randomnum = Math.floor(Math.random() * 6) + 1; // creating random between 1-6 (+1 because without +1, it will randomize 0-5)
+    dicevalue.push(randomnum);
+  }
+  return dicevalue;
+}
+
 export default function App(){
   return (
     <>
