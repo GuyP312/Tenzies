5653788e6427e59f0f9de0e8e793528d40e755dd
GuyP <guyparnchinda@gmail.com>
2024-12-24 02:41:02 +0700

    import nanoid to give key to die component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import Die from './Components/Die';
+import { nanoid } from "nanoid";
 export default function App(){
   function generateAllValueDice(){ // return an array of random numbers between 1-6 of length 10
     const dicevalue = [];
     for(let i=0; i<10;i++){
       const randomnum = Math.floor(Math.random() * 6) + 1; // creating random number between 1-6 (+1 because without +1, it will randomize 0-5)
-      dicevalue.push({value:randomnum,isHeld: false});
+      dicevalue.push({value:randomnum,isHeld: false,id:nanoid()});
     }
     return dicevalue;
   }
   const [dicevalues, setdicevalues] = React.useState(generateAllValueDice); // set the array value to equal a state and then map it and send in random value as prop
   const displaydice = dicevalues.map((die)=>{
-    return <Die number={die.value}/>
+    return <Die key = {die.id} number={die.value}/>
   })
 
   function newdice(){ // set the array to have new value and then update state
