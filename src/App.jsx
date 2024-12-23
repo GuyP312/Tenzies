7d800c01bf71e8cb761a79c98abf829ff5decdc0
GuyP <guyparnchinda@gmail.com>
2024-12-23 22:25:24 +0700

    Call 10 Die components in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import Die from './Components/Die';
 
 export default function App(){
   return (
     <>
-      <main></main>
+      <main>
+        <Die number={1} />
+        <Die number={2} />
+        <Die number={3} />
+        <Die number={4} />
+        <Die number={5} />
+        <Die number={6} />
+        <Die number={7} />
+        <Die number={8} />
+        <Die number={9} />
+        <Die number={10} />
+      </main>
     </>
   )
 }
---
+++ src/Components/Die.jsx
@@ -0,0 +1,7 @@
+export default function Die(props){
+    return(
+        <>
+            <h1>{props.number}</h1>
+        </>
+    )
+}
